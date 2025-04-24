65d860f0d219eb3992020e5429906b1ad3e3424d
lyh <asgard@asgard.com>
2025-04-24 16:40:34 +0900

    start

--- threeStar/src/main/java/com/kh/tt/profile/controller/ProfileController.java
+++ threeStar/src/main/java/com/kh/tt/profile/controller/ProfileController.java
@@ -16,23 +16,23 @@ public class ProfileController {
 	private ProfileServiceImpl pService;
 	
 	@RequestMapping("profile.do") // 영훈 씨 모달용
-	public String selectProfile() {
+//	public String selectProfile() {
 //	public String selectProfile (@RequestParam(value="memNo", required=true) int memNo, Model model) {
 //		if(memNo <= 0) {
 //			model.addAttribute("errorMsg", "멤버가 아닙니다");
 //			return "common/mainPage";
 //		}
-//	public String selectProfile (@RequestParam(value="memNo") int memNo, Model model) {	
-//		int result = pService.selectProfile(memNo);
-//		
-//		System.out.println("controller : " + result);
-//		
-//		if(result > 0) {
+	public String selectProfile (@RequestParam(value="memNo") int memNo, Model model) {	
+		int result = pService.selectProfile(memNo);
+		
+		System.out.println("controller : " + result);
+		
+		if(result > 0) {
 			return "profile/profile";
-//		}else {
-//			model.addAttribute("errorMsg", "조회 실패");
-//			return "common/mainPage";
-//		}
+		}else {
+			model.addAttribute("errorMsg", "조회 실패");
+			return "common/mainPage";
+		}
 	}
 	
 	@RequestMapping("detailProfile.do")
