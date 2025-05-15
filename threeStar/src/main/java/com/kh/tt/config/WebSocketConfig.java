3ae7ee531fcb3448bd34f6097b017b3162563b70
eastjin616 <djseo9812@gmail.com>
2025-05-15 18:10:42 +0900

    test

--- threeStar/src/main/java/com/kh/tt/config/WebSocketConfig.java
+++ threeStar/src/main/java/com/kh/tt/config/WebSocketConfig.java
@@ -21,7 +21,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(chatWebSocketHandler, "ws/chat/{roomId}")
+        registry.addHandler(chatWebSocketHandler, "/ws/chat/{roomId}")
         .setAllowedOrigins("*")
         .addInterceptors(new HttpSessionHandshakeInterceptor());
     }
